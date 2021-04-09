6e1a628be7db5919dd8125e660b40fcb7bc5a73e
kkrr10 <>
2021-04-09 18:32:42 +0900

    20210409

---
+++ ABC075/a.java/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        if(a == b) System.out.println(c);
+        else if(b == c) System.out.println(a);
+        else System.out.println(b);
+    }
+}
