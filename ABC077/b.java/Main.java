fb269f0df7ec8cbd012d521f2191891ea565540f
kkrr10 <rkorekata@keio.jp>
2021-07-07 07:30:26 +0900

    20210707

---
+++ ABC077/b.java/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)Math.pow((double)(int)Math.sqrt(sc.nextDouble()), 2));
+    }
+}
