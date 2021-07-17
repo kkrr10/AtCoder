472096c777da1819659d6d15d9855860760ea068
kkrr10 <rkorekata@keio.jp>
2021-07-17 23:12:55 +0900

    20210717

---
+++ ABC062/b.java/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), w = sc.nextInt();
+        for(int i=0; i<w+2; i++) System.out.print("#");
+        System.out.println();
+        for(int i=0; i<h; i++) System.out.println("#" + sc.next() + "#");
+        for(int i=0; i<w+2; i++) System.out.print("#");
+        System.out.println();
+    }
+}
