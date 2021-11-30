36dcf213efb70e00d353b050754d00a1a81eb23b
kkrr10 <rkorekata@keio.jp>
2021-11-30 23:53:14 +0900

    20211130

---
+++ ABC210/b.kt
@@ -0,0 +1,4 @@
+fun main() {
+    readLine()
+    println(if (readLine()!!.indexOf('1') % 2 == 0) "Takahashi" else "Aoki")
+}
---
+++ ABC210/b2.kt
@@ -0,0 +1,4 @@
+fun main() {
+	readLine()
+	println(listOf("Takahashi", "Aoki")[readLine()!!.indexOf("1")%2])
+}
